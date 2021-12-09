c4c701f8c0ea2287054ca5689c1ddcb5ed10fbc4
Gadise Mustika <gadisee6@gmail.com>
2021-12-09 15:15:20 +0700

    making class and object in produk.php and coba.php

---
+++ coba.php
@@ -0,0 +1,12 @@
+<?php
+// membuat class coba 
+class Coba{
+
+}
+// membuat objek - > menginstance (implementasi) dari class
+$coba1 = new Coba();
+$coba2 = new Coba();
+
+var_dump($coba1);
+
+?>
---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php
+// Jualan Produk
+// 1. Komik
+// 2.Games
+
+class Produk{
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+?>
