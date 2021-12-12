d50e935bb91ff52b85bc3b488a87aa730081559c
Gadise Mustika <gadisee6@gmail.com>
2021-12-12 15:37:31 +0700

    oop without inheritance

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -5,33 +5,42 @@
 
 class Produk
 {
-    public $judul,
+    public $jenis,
+           $judul,
            $penulis,
            $penerbit,
            $harga,
            $waktuMain,
            $jumlahHalaman;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0,$jumlahHalaman = 0,$waktuMain = 0)
+    public function __construct($jenis = "jenis",$judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0,$jumlahHalaman = 0,$waktuMain = 0)
     {
+        $this->jenis = $jenis;
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
-        $this->waktuMain = $waktuMain;
         $this->jumlahHalaman = $jumlahHalaman;
+        $this->waktuMain = $waktuMain;
         
     }
 
     // 1.membuat method untuk mencetak produk
     public function getLabel()
     {
-        return "$this->judul,$this->penulis, $this->penerbit";
+        return "$this->penulis, $this->penerbit";
     }
 
     // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30000) - 100 halaman
     public function tampilProduk(){
-        return $str = ""
+        $str = "{$this->jenis} : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
+        if($this->jenis == "Komik"){        
+           $str.= " - {$this->jumlahHalaman} Halaman";
+        }else{
+          $str .= " ~  {$this->waktuMain} Jam" ;
+        }
+
+        return $str;
     }
 }
 
@@ -42,17 +51,14 @@ class CetakInfoProduk
     {
         // fungsi ini akan mengembalikan nilai string yang akan memberikan informasi cetak produk
         // berdasarkan property class Produk
-        return  $str = "{$produk->judul} | {$produk->getLabel()} (Rp.{$produk->harga})";
+        return  $str = "{$produk->judul} | {$produk->getLabel()} (Rp.{$produk->harga}) ";
     }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000,100,0);
+$produk1 = new Produk("Komik","Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100,0);
 
-$produk2 = new Produk("HarvestMoon", "Anonymous", "Natsume", "100000",0,20);
+$produk2 = new Produk("Game","HarvestMoon", "Anonymous", "Natsume", "100000", 0, 20);
 
-// Naruto | Naruto,Masashi Kishimoto, Shonen Jump (Rp.30000)
-// Komik: Naruto,Masashi Kishimoto, Shonen Jump
-// Game: HarvestMoon,Anonymous, Natsume
-
-// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp.30000) - 100 halaman
-// Game : HarvestMoon | Anonymous, Natsume (Rp.100000) - 20 Jam
+echo $produk1->tampilProduk();
+echo "<br>";
+echo $produk2->tampilProduk();
